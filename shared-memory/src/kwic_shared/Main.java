3e233d92f023468dcf9315bed9f75d7cacf011a2
Alenka - MB Pro <alenka@alenka-pc.(none)>
2011-02-24 08:07:35 +0100

    main class structure

---
+++ shared-memory/src/kwic_shared/Main.java
@@ -0,0 +1,30 @@
+package kwic_shared;
+
+public class Main {
+	public static void main(String[] args) {
+		manageInput();
+		doCircularShift();
+		doAlphabetization();
+		manageOutput();
+	}
+
+	private static void manageInput() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void doCircularShift() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void doAlphabetization() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void manageOutput() {
+		// TODO Auto-generated method stub
+		
+	}
+}
